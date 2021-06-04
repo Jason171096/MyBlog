4fdc74562c20b1cefa580174a65ee40bc9677b35
Jason117 <bboyace10@gmail.com>
2021-06-03 19:52:58 -0500

    New design in Header

--- js/preLoader.js
+++ js/preLoader.js
@@ -1,17 +1,18 @@
 const preLoader = document.getElementById('preLoader');
+const preLoaderLogo = document.getElementById('preLoaderLogo');
 const body = document.querySelector('body');
 
 const css = window.document.styleSheets[0];
 css.insertRule(`
 @keyframes swirl {
-    0% {
-      transform: rotate(0) scale(1);
-      opacity: 1;
-    }
-    100% {
-      transform: rotate(-540deg) scale(0);
-      opacity: 0;
-    }
+  0% {
+    transform: rotate(0) scale(1);
+    opacity: 1;
+  }
+  100% {
+    transform: rotate(-540deg) scale(0);
+    opacity: 0;
+  }
 }`, css.cssRules.length);
 
 const timeOut = () => {
@@ -21,7 +22,10 @@ const timeOut = () => {
 }
 
 preLoader.addEventListener('click', () => {
-    preLoader.style.animation = 'swirl 1s ease-in both'
+  preLoader.style.animation = 'swirl 1s ease-in both'
+  console.log("Si")
+  // preLoaderLogo.style.animationIterationCount = '0'
+  // preLoaderLogo.style.animation = 'slide .45s cubic-bezier(0.470, 0.000, 0.745, 0.715) both'
     setTimeout(timeOut, 1000)
 })
 
