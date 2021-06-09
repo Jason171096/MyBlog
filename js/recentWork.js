4e055e290f2d3132acc0614e4a81a64eedcad00e
Jason117 <bboyace10@gmail.com>
2021-06-09 16:45:57 -0500

    Buttons added

---
+++ js/recentWork.js
@@ -0,0 +1,14 @@
+let work1 = document.querySelector(".grid-work1")
+work1.addEventListener("click", () => {
+    window.open("https://hulu-cyan.vercel.app/", "_blank")
+})
+
+let work2 = document.querySelector(".grid-work2")
+work2.addEventListener("click", () => {
+    window.open("https://dollapractice.netlify.app/", "_blank")
+})
+
+let work3 = document.querySelector(".grid-work3")
+work3.addEventListener("click", () => {
+    window.open("https://batabitpractice.netlify.app/", "_blank")
+})
